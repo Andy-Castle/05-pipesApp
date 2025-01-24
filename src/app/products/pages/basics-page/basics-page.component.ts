d99dd0ce95ad1b41ea16d295c2e810222ad581fa
Andy Castle <masterchief_and@live.com>
2025-01-24 15:09:01 -0500

    se vio el pipe de uppercase, lowercase y titlecase, solo modifican visualmente la data, no hacen nada mas con ella

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'andy';
+  public nameUpper: string = 'ANDY';
+  public fullName: string = 'aNdY cAsTLe';
 }
