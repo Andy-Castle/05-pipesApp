5aa8448852c015fa5ddc0781c6b7605b2ea607ff
Andy Castle <masterchief_and@live.com>
2025-01-27 10:11:58 -0500

    i18Select: Se vio este pipe el cual tiene como función "actuar" como un ngif, pero debemos realizar un map, en este caso fue de genero, si es hombre o mujer este le apareceria la palabra invitarlo u invitarla

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  //i18n Select
+  public name: string = 'Andy';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Frida';
+    this.gender = 'female';
+  }
 }
