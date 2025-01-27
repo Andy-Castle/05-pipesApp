3d0d296b64d9e3b85b9d53d594ada27b598f0427
Andy Castle <masterchief_and@live.com>
2025-01-27 10:49:52 -0500

    async pipe: Se vio como funciona el pipe en los observables y en las promesas y como en el observable si nos salimos donde se ejecuta este, se desuscribe, mientras que en la promesa esta sigue funcionando

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -39,4 +40,17 @@ export class UncommonPageComponent {
     age: '25',
     address: 'México',
   };
+
+  //Async pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap((value) => console.log('tap', value))
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa');
+      console.log('Tenemos data en la promesa');
+      this.person.name = 'Otro nombre';
+    }, 3500);
+  });
 }
